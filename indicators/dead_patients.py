20f1583979014e9f5a627631b56b3a432755fa24
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-02-26 16:02:20 +0100

    Implement DeadPatientsDuringPeriod indicator

--- indicators/dead_patients.py
+++ indicators/dead_patients.py
@@ -24,13 +24,31 @@ class DeadPatients(PatientIndicator):
             age_is_null=age_is_null,
             include_null_dates=include_null_dates
         )
-        return patients[pd.notnull(patients['dead'])]
+        is_dead = pd.notnull(patients['dead'])
+        is_dead_before_limit = patients['dead'] <= limit_date
+        return patients[is_dead & is_dead_before_limit]
 
 
-class DeadPatientsAtPeriod(DeadPatients):
+class DeadPatientsDuringPeriod(PatientIndicator):
     """
-    Indicator that compute the number of patients who died at the given
-    period.
+    Indicator that compute the number of patients who died during the given
+    period (between start_date and limit_date).
     """
 
-    pass
+    def get_filtered_patients_dataframe(self, limit_date, start_date=None,
+                                        gender=None, age_min=None,
+                                        age_max=None, age_is_null=False,
+                                        include_null_dates=False):
+        patients = self.filter_patients_by_category(
+            limit_date,
+            start_date=None,
+            gender=gender,
+            age_min=age_min,
+            age_max=age_max,
+            age_is_null=age_is_null,
+            include_null_dates=include_null_dates
+        )
+        is_dead = pd.notnull(patients['dead'])
+        is_dead_before_limit = patients['dead'] <= limit_date
+        is_dead_after_start = patients['dead'] >= start_date
+        return patients[is_dead & is_dead_before_limit & is_dead_after_start]
