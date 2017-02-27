42e1d1cd2425636eb8eed9d50567d6f9a5d4273d
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-02-27 11:25:51 +0100

    Implement base template processor skeleton

--- template_processor/base_template_processor.py
+++ template_processor/base_template_processor.py
@@ -1,1 +1,28 @@
 # coding: utf-8
+
+
+class BaseTemplateProcessor:
+    """
+    Abstract base class for report template processing.
+    """
+
+    def __init__(self, template_path):
+        self.template_path = template_path
+
+    def get_indicator_for_cell(self, i, j):
+        raise NotImplementedError()
+
+    def get_parameters_for_cell(self, i, j):
+        raise NotImplementedError()
+
+    def get_cell_value(self, start_date, end_date, i, j):
+        raise NotImplementedError()
+
+    def get_cell_values(self, start_date, end_date):
+        raise NotImplementedError()
+
+    def get_column_number(self):
+        raise NotImplementedError()
+
+    def get_row_number(self):
+        raise NotImplementedError()
