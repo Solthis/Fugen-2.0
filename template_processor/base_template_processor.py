7a1ed1db9bcc64ef6742957b52224703420bca96
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-02-27 12:27:44 +0100

    Start base template processor implementation

--- template_processor/base_template_processor.py
+++ template_processor/base_template_processor.py
@@ -1,5 +1,9 @@
 # coding: utf-8
 
+import re
+
+import numpy as np
+
 
 class BaseTemplateProcessor:
     """
@@ -9,17 +13,30 @@ class BaseTemplateProcessor:
     def __init__(self, template_path):
         self.template_path = template_path
 
-    def get_indicator_for_cell(self, i, j):
+    def get_cell_content(self, i, j):
         raise NotImplementedError()
 
-    def get_parameters_for_cell(self, i, j):
-        raise NotImplementedError()
+    def get_cell_members(self, i, j):
+        regex = "^\{(.+?)\}$"
+        res = re.findall(regex, self.get_cell_content(i, j))
+        return res[0].split(";")
+
+    def get_cell_indicator(self, i, j):
+        pass
+
+    def get_cell_parameters(self, i, j):
+        pass
 
     def get_cell_value(self, start_date, end_date, i, j):
         raise NotImplementedError()
 
     def get_cell_values(self, start_date, end_date):
-        raise NotImplementedError()
+        matrix = np.empty((self.get_row_number(), self.get_column_number()))
+        matrix[:] = np.NAN
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                matrix[i, j] = self.get_cell_value(start_date, end_date, i, j)
+        return matrix
 
     def get_column_number(self):
         raise NotImplementedError()
