24f5a13ca8641a2aacb0725346bca37fc3e28806
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-03-03 17:17:40 +0100

    Implement arv started during period

--- indicators/arv_started_patients.py
+++ indicators/arv_started_patients.py
@@ -47,3 +47,51 @@ class ArvStartedPatients(PatientIndicator):
             [df1['patient_id'], df3['patient_id']]
         ).unique()
         return patients[patients['id'].isin(patient_ids)]
+
+
+class ArvStartedDuringPeriod(PatientIndicator):
+    """
+    Indicator that computes the number of patients who started an ARV
+    treatment during the given period.
+    """
+
+    @classmethod
+    def get_key(cls):
+        return "ARV_STARTED_DURING_PERIOD"
+
+    def filter_patients_dataframe(self, limit_date, start_date=None,
+                                  include_null_dates=False):
+        patients = self.filter_patients_by_category(
+            limit_date,
+            start_date=None,
+            include_null_dates=include_null_dates
+        )
+        patient_drugs = self.filter_patient_drugs_by_category(
+            limit_date,
+            start_date=None,
+            include_null_dates=include_null_dates
+        )
+        visit_drugs = self.filter_visit_drugs_by_category(
+            limit_date,
+            start_date=None,
+            include_null_dates=include_null_dates
+        )
+        visits = self.filter_visits_by_category(
+            limit_date,
+            start_date=None,
+            include_null_dates=include_null_dates
+        )
+        filter1 = ~patient_drugs['drug_id'].isin(constants.EXCLUDED_DRUGS)
+        df1 = patient_drugs[filter1]
+        filter2 = ~visit_drugs['drug_id'].isin(constants.EXCLUDED_DRUGS)
+        df2 = visit_drugs[filter2]
+        df3 = visits[visits['id'].isin(df2['visit_id'])]
+        s1 = df1.groupby('patient_id')['beginning'].min()
+        s2 = df3.groupby('patient_id')['visit_date'].min()
+        s = pd.concat([s1, s2])
+        s_grouped = s.groupby(s.index).min()
+        c = (s_grouped >= start_date) & (s_grouped <= limit_date)
+        s_period = s_grouped[c]
+        if len(s_period) == 0:
+            return patients[:0]
+        return patients.loc[s_period.index]
