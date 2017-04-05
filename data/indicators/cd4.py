a1facaa03bb49e9406e568f9dd99ed9aa56bd023
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-04-05 11:33:09 +0200

    Add licence headers

--- data/indicators/cd4.py
+++ data/indicators/cd4.py
@@ -1,5 +1,23 @@
 # coding: utf-8
 
+# Copyright 2017 Solthis.
+#
+# This file is part of Fugen 2.0.
+#
+# Fugen 2.0 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fugen 2.0 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fugen 2.0. If not, see <http://www.gnu.org/licenses/>.
+
+
 import pandas as pd
 
 from data.indicators.patient_indicator import PatientIndicator
